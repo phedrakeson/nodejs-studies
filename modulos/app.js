bc625de416fb5d40d5c5b171e5ba1461c1958ae0
Gabriel Duarte <gabrield.developer@gmail.com>
2022-01-24 20:08:16 -0300

    imports, dirname e filename

--- modulos/app.js
+++ modulos/app.js
@@ -2,7 +2,9 @@ const mod1 = require('./module_1');
 const falaNome = require('./module_1').falaNome;
 const { nome, sobrenome } = require('./module_1');
 const axios = require('axios');
-const { Pessoa } = require('./module_1')
+const { Pessoa } = require('./module_1');
+const multiplicacao = require('./module_2');
+const path = require('path');
 
 axios('https://www.otaviomiranda.com.br/files/json/pessoas.json')
 .then(res => console.log(res.data))
@@ -11,3 +13,8 @@ axios('https://www.otaviomiranda.com.br/files/json/pessoas.json')
 const p1 = new Pessoa('Luiz');
 console.log(p1);
 console.log(mod1);
+console.log(multiplicacao(2, 2));
+console.log(__filename);
+console.log(__dirname);
+console.log(path.resolve(__dirname));
+console.log(path.resolve(__dirname, '..', '..', 'pictures'));
