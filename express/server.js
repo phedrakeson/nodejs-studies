fedc761a5a33b45e1f9afa166ee05f1e79da15e2
Gabriel Duarte <gabrield.developer@gmail.com>
2022-01-30 15:12:48 -0300

    adiciona dotenv, realiza primeira conexao e cria model com MongoDB

--- express/server.js
+++ express/server.js
@@ -1,5 +1,16 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.CONNECTIONSTRING)
+  .then(() => {
+    app.emit(true);
+  })
+  .catch((e) => console.error(`Inicialização falhou: ${e}`));
+  
+
 const routes = require('./routes');
 const path = require('path');
 const middleware = require('./src/middlewares/middleware');
@@ -11,7 +22,9 @@ app.use(express.urlencoded({extended: true}))
 app.use(middleware);
 app.use(routes);
 
-app.listen(3000, () => {
-  console.log('Acesse http://localhost:3000');
-  console.log('Servidor rodando...');
-});
+app.on(true, () => {
+  app.listen(3000, () => {
+    console.log('Acesse http://localhost:3000');
+    console.log('Servidor rodando...');
+  });
+})
