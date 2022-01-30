e8802400f65b99de1a271b05b6d54962ea612857
Gabriel Duarte <gabrield.developer@gmail.com>
2022-01-30 13:35:55 -0300

    implementa middleware para rotas

--- express/server.js
+++ express/server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const routes = require('./routes');
 const path = require('path');
+const middleware = require('./src/middlewares/middleware');
 
 app.use(express.static(path.resolve(__dirname, 'public')));
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}))
+app.use(middleware);
 app.use(routes);
 
 app.listen(3000, () => {
