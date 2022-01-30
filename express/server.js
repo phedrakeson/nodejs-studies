2b73473b11a1b2c8f32b2df919b43b84ec2080a6
Gabriel Duarte <gabrield.developer@gmail.com>
2022-01-30 12:35:51 -0300

    utilizando arquivos estaticos com express

--- express/server.js
+++ express/server.js
@@ -3,6 +3,7 @@ const app = express();
 const routes = require('./routes');
 const path = require('path');
 
+app.use(express.static(path.resolve(__dirname, 'public')));
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}))
