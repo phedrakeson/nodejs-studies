bf286c2eea06f0fd5881445ed52eab28511f9b69
Gabriel Duarte <gabrield.developer@gmail.com>
2022-01-30 11:31:03 -0300

    routes e controllers

--- express/server.js
+++ express/server.js
@@ -1,29 +1,9 @@
 const express = require('express');
 const app = express();
+const routes = require('./routes');
 
 app.use(express.urlencoded({extended: true}))
-
-app.get('/', (req, res) => {
-  res.send(`<form action="/" method="POST">
-  Nome: <input type="text" name="nome">
-  <button>Enviar</button>
-  </form>`);
-});
-
-app.post('/', (req, res) => {
-  console.log(req.body)
-  res.send('Recebi o formulario')
-});
-
-app.get('/testes/:id?', (req, res) => {
-  console.log(req.params)
-  console.log(req.query)
-  res.send(`O seu ID é: ${req.params.id}`)
-});
-
-app.get('/contato', (req, res) => {
-  res.send('lorem ipsum')
-})
+app.use(routes);
 
 app.listen(3000, () => {
   console.log('Acesse http://localhost:3000');
